d00e06fad251ac6ca320cae1215fe605de6ab13a
bharatanand <bharatanand@outlook.in>
2022-08-18 15:46:05 +0530

    yet another code

--- Day2/day2_.py
+++ Day2/day2_.py
@@ -1,1 +1,14 @@
-print("\n   Hello world")
+# DataTypes :) 
+
+#string
+
+print("Hello"[0])    # Subscripting Concept :  here the [] tell the user which position of the string is pointed out 
+print("Hello"[4])
+
+#INTEGER
+
+print(123 + 22) 
+
+#float : "2.2" 
+
+#Boolean : "True" "False"  
